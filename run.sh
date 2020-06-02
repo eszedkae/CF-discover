8cb98a8ebec9137d453e1879fcc67f677fd32949
eszedkae <eszedkae@gmail.com>
2020-06-02 19:23:22 +0500

    Initial commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $1 == "setup" ]; then
+    git clone https://github.com/HatBashBR/HatCloud.git
+    git clone https://github.com/vincentcox/bypass-firewalls-by-DNS-history.git
+    git clone https://github.com/m0rtem/CloudFail.git
+    git clone https://github.com/christophetd/CloudFlair.git
+    pip3 install -r CloudFail/requirements.txt
+    pip install -r cloudflair/requirements.txt
+    apt install ruby -y
+
+elif [ $1 == "scan"]; then
+    python3 CloudFail/cloudfail.py --target $2
+    python cloudflair/cloudflair.py $2
+    bash bypass-firewalls-by-DNS-history/bypass-firewalls-by-DNS-history.sh -d $2
+    ruby HatCloud/hatcloud.rb -b $2
