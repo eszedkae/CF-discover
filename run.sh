7b803522425c3db01d9a5f804576355f90db7fdd
eszedkae <eszedkae@gmail.com>
2020-06-02 19:47:34 +0500

    fix

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ then
     git clone https://github.com/christophetd/CloudFlair.git
     pip3 install -r CloudFail/requirements.txt
     pip install -r cloudflair/requirements.txt
-    apt install ruby -y
+    apt install ruby, jq -y
 
 elif [ $1 = "scan" ]
 then
-    cd CloudFail/ && python3 cloudfail.py --target $2 && cd ..
-    cd cloudflair/ && python cloudflair.py $2 && cd ..
-    cd bypass-firewalls-by-DNS-history/ && bash bypass-firewalls-by-DNS-history.sh -d $2 && cd ..
-    cd HatCloud/ && ruby hatcloud.rb -b $2 && cd ..
+    cd CloudFail/ && python3 cloudfail.py --target $2 && cd .. && \
+    cd cloudflair/ && python cloudflair.py $2 && cd .. && \
+    cd bypass-firewalls-by-DNS-history/ && bash bypass-firewalls-by-DNS-history.sh -d $2 && cd .. && \
+    cd HatCloud/ && ruby hatcloud.rb -b $2 && cd .. 
 fi
